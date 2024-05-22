84b6e1e9450a242913ead11fcfb4036aab958446
Judith Buck <judith18buckden@hotmail.com>
2024-05-22 21:43:45 +0100

    Add project files.

---
+++ HexadecimalConvertTemplate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexadecimalConvertTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // hexadecimal - denary conversion
+            //define array of hexadecimal values
+
+
+            //convert two random denary numbers to hexadecimal (index value of hexadecimal values array)
+
+
+            //convert hexadecimal number to denary 
+
+
+            //output hexadecimal and denary numbers
+
+        }
+    }
+}
